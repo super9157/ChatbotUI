48d532e2186712d364a2de96aaaefbd20cf10a33
Super <newest.star1990@gmail.com>
2024-10-25 01:43:50 -0400

    feat: added create button and search
    Co-authored-by: Mckay Wrigley <29221284+mckaywrigley@users.noreply.github.com>

---
+++ components/sidebar/sidebar-search.tsx
@@ -0,0 +1,26 @@
+import { ContentType } from "@/types"
+import { FC } from "react"
+import { Input } from "../ui/input"
+import { useTranslation } from "react-i18next"
+
+interface SidebarSearchProps {
+  contentType: ContentType
+  searchTerm: string
+  setSearchTerm: Function
+}
+
+export const SidebarSearch: FC<SidebarSearchProps> = ({
+  contentType,
+  searchTerm,
+  setSearchTerm
+}) => {
+  const { t } = useTranslation()
+
+  return (
+    <Input
+      placeholder={t(`Search ${contentType}...`)}
+      value={searchTerm}
+      onChange={e => setSearchTerm(e.target.value)}
+    />
+  )
+}
