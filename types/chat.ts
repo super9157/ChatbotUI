50ccc6552eb19b976620ef026c3a3711e0c57512
Super <newest.star1990@gmail.com>
2024-11-12 00:43:47 -0500

    feat: added chat types
    Co-authored-by: Mckay Wrigley <29221284+mckaywrigley@users.noreply.github.com>

---
+++ types/chat.ts
@@ -0,0 +1,27 @@
+import { Tables } from "@/supabase/types"
+import { ChatMessage, LLMID } from "."
+
+export interface ChatSettings {
+  model: LLMID
+  prompt: string
+  temperature: number
+  contextLength: number
+  imageSize: "1024x1024" | "1024x1792" | "1792x1024" | null
+  includeProfileContext: boolean
+  includeWorkspaceInstructions: boolean
+  embeddingsProvider: "openai" | "local"
+}
+
+export interface ChatPayload {
+  chatSettings: ChatSettings
+  workspaceInstructions: string
+  chatMessages: ChatMessage[]
+  assistant: Tables<"assistants"> | null
+  messageFileItems: Tables<"file_items">[]
+  chatFileItems: Tables<"file_items">[]
+}
+
+export interface ChatAPIPayload {
+  chatSettings: ChatSettings
+  messages: Tables<"messages">[]
+}
