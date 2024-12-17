fea70b80396f99f081f699d22c6d9904e0591b72
Super <newest.star1990@gmail.com>
2024-12-17 00:28:14 -0500

    feat: added command api
    Co-authored-by: Mckay Wrigley <29221284+mckaywrigley@users.noreply.github.com>

---
+++ app/api/command/route.ts
@@ -0,0 +1,52 @@
+import { CHAT_SETTING_LIMITS } from "@/lib/chat-setting-limits"
+import OpenAI from "openai"
+
+export const runtime = "edge"
+
+export async function POST(request: Request) {
+  const json = await request.json()
+  const { input } = json as {
+    input: string
+  }
+
+  try {
+    const apiKey = process.env.OPENAI_API_KEY || ""
+
+    if (!apiKey) {
+      throw new Error("OpenAI API Key not found")
+    }
+
+    const openai = new OpenAI({ apiKey })
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-4-1106-preview",
+      messages: [
+        {
+          role: "system",
+          content: "Respond to the user."
+        },
+        {
+          role: "user",
+          content: input
+        }
+      ],
+      temperature: 0,
+      max_tokens:
+        CHAT_SETTING_LIMITS["gpt-4-turbo-preview"].MAX_TOKEN_OUTPUT_LENGTH
+      //   response_format: { type: "json_object" }
+      //   stream: true
+    })
+
+    const content = response.choices[0].message.content
+
+    return new Response(JSON.stringify({ content }), {
+      status: 200
+    })
+  } catch (error: any) {
+    const errorMessage = error.error?.message || "An unexpected error occurred"
+    const errorCode = error.status || 500
+    return new Response(JSON.stringify({ message: errorMessage }), {
+      status: errorCode
+    })
+  }
+}
